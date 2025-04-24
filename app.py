21f17837f0970fd34d0f2191d58b04011492e446
Rosalie337 <rosalie337@gmail.com>
2025-04-24 16:09:01 -0700

    prints a string

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+print("My friend's name is George.")
+print("George enjoys research and coding.")
+print("George is 20 years old and is a student.")
+print("George is 10 years younger than me.")
