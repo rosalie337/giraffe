c1d0767a40d3a0362758b499304f97de26921cea
Rosalie337 <rosalie337@gmail.com>
2025-04-24 16:19:23 -0700

    added more variables

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ character_age = "50"
 
 print("My friend's name is " + character_name + ".")
 print("" + character_name + " enjoys research and coding.")
-print("George is " + character_age + " years old and is a student.")
-print("George is " + character_age + " years younger than me.")
+print("" +character_name + " is " + character_age + " years old and is a student.")
+
+character_name =  "Tom"
+character_age = "22"
+
+print("" +character_name + " is " + character_age + " years younger than me.")
