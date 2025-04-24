c5f57617a96ca797ca9977b96d28d111dfe46441
Rosalie337 <rosalie337@gmail.com>
2025-04-24 16:57:37 -0700

    spring w/ input.

--- app.py
+++ app.py
@@ -1,14 +1,2 @@
-character_name = "Mike"
-character_age = "50"
-
-print("My friend's name is " + character_name + ".")
-print("" + character_name + " enjoys research and coding.")
-print("" +character_name + " is " + character_age + " years old and is a student.")
-
-character_name =  "Tom"
-character_age = "22"
-
-print("" +character_name + " is " + character_age + " years younger than me.")
-
-phrase = "Coding Academy"
-print(phrase.islower())
+name = input("Enter your name: ")
+print ("Hello " + name + "!")
